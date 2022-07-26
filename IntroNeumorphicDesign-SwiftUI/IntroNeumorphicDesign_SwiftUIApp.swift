8859aeb805003e86016499051069fdc34f5bd9fb
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-26 12:25:29 -0700

    Initial Commit

---
+++ IntroNeumorphicDesign-SwiftUI/IntroNeumorphicDesign_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroNeumorphicDesign_SwiftUIApp.swift
+//  IntroNeumorphicDesign-SwiftUI
+//
+//  Created by Mitya Kim on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct IntroNeumorphicDesign_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
